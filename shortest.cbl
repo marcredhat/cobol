8b2fc7a6fe71738f5da748c9d3348503979b1a0c
Marc Chisinevski <mchisine@redhat.com>
2020-04-12 21:23:07 -0700

    Update shortest.cbl

--- shortest.cbl
+++ shortest.cbl
@@ -13,10 +13,10 @@ WORKING-STORAGE SECTION.
 01  Result                              PIC 99 VALUE ZEROS.
 
 PROCEDURE DIVISION.
-    DISPLAY "Enter first number  (1 digit) : " WITH NO ADVANCING.
-    ACCEPT Num1.
-    DISPLAY "Enter second number (1 digit) : " WITH NO ADVANCING.
-    ACCEPT Num2.
-    MULTIPLY Num1 BY Num2 GIVING Result.
+    *> DISPLAY "Enter first number  (1 digit) : " WITH NO ADVANCING.
+    *> ACCEPT Num1.
+    *> DISPLAY "Enter second number (1 digit) : " WITH NO ADVANCING.
+    *> ACCEPT Num2.
+    MULTIPLY 5 BY 10 GIVING Result.
     DISPLAY "Result is = ", Result.
     STOP RUN.
